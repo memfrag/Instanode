36de6b67fed1e41b135c7e5618a6274a6d317a82
Martin Johannesson <martin.johannesson@bontouch.com>
2013-07-09 11:05:47 +0200

    Updated radius to 5 kilometers.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function instagramHandler(query, request, response) {
 var server = AppServer.createAppServer(requestHandlers, __dirname + "/static");
 server.startOnPort(13337);
 
-Instagram.periodicallyGetImagesByLocation(59.3300, 18.0700, 2000, function (jsonOutput) {
+Instagram.periodicallyGetImagesByLocation(59.3300, 18.0700, 5000, function (jsonOutput) {
 	console.log("*** Got new images from Stockholm.");
 	stockholmImages = jsonOutput;
 });
