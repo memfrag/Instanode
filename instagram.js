a1237345d2446a46ecf3c63566a97249cc37b287
Martin Johannesson <martin.johannesson@bontouch.com>
2013-07-09 10:47:33 +0200

    Now reads Instagram client ID from instagram.txt.

--- instagram.js
+++ instagram.js
@@ -1,9 +1,20 @@
 var Https = require('https');
+var Fs = require('fs');
 
 exports.getImagesByLocation = getImagesByLocation;
 exports.periodicallyGetImagesByLocation = periodicallyGetImagesByLocation;
 
-var instagramClientId = "instagramClientIdGoesHere";
+var instagramClientId = readInstagramClientId();
+
+function readInstagramClientId() {
+	var clientId = Fs.readFileSync(__dirname + "/instagram.txt", "utf8");
+	if (clientId) {
+		return clientId.trim();		
+	} else {
+		console.log("*** Error: Unable to read Instagram client ID from file instagram.txt");
+		return null;
+	}
+}
 
 function getImagesByLocation(latitude, longitude, distance, completionCallback, errorCallback) {
 	Https.get('https://api.instagram.com/v1/media/search?client_id='
@@ -13,7 +24,7 @@ function getImagesByLocation(latitude, longitude, distance, completionCallback, 
 		+ '&distance=' + distance,
 		function(res) {
 		// FIXME: We should really check the res.statusCode here...
-		if (res.statusCode) {
+		if (res.statusCode != 200) {
 			errorCallback("Unable to get images, HTTP status code " + res.statusCode);
 			return;
 		}		
